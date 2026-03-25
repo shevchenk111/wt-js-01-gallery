c0a57743df46fb4412197b54b4c141b37c59c9fa
sheremet <danasevcenko695@gmail.com>
2026-03-25 23:23:42 +0200

    Delete task-2.js

--- js/task-2.js
+++
@@ -1,8 +0,0 @@
-const total = 100;
-const ordered = 50;
-
-if (ordered > total) {
-  console.log("На складі недостатньо товарів!");
-} else {
-  console.log("Замовлення оформлено, з вами зв'яжеться менеджер");
-}
