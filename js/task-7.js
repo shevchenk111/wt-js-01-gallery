cb744b92b978b710ebbbac8333f8bf2f271792a9
sheremet <danasevcenko695@gmail.com>
2026-03-25 23:01:17 +0200

    Create task-7.js

---
+++ js/task-7.js
@@ -0,0 +1,18 @@
+function checkStorage(available, ordered) {
+  let message;
+
+  if (ordered === 0) {
+    message = "У замовленні немає товарів";
+  } else if (ordered > available) {
+    message = "Ваше замовлення занадто велике, на складі недостатньо товарів!";
+  } else {
+    message = "Замовлення прийнято, наш менеджер зв'яжеться з Вами.";
+  }
+
+  return message;
+}
+
+// приклади перевірки
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(70, 0));
