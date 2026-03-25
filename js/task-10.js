83bd744ee12c76d84934feaef9fb0d660a498e0f
sheremet <danasevcenko695@gmail.com>
2026-03-25 23:01:08 +0200

    Create task-10.js

---
+++ js/task-10.js
@@ -0,0 +1,27 @@
+const formatString = function (string) {
+  if (string.length <= 40) {
+    return string;
+  } else {
+    return string.slice(0, 40) + "...";
+  }
+};
+
+/*
+ * Викличте функції для перевірки працездатності вашої реалізації.
+ */
+
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// повернеться оригінальний рядок
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// повернеться форматований рядок
+
+console.log(formatString('Curabitur ligula sapien.'));
+// повернеться оригінальний рядок
+
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
+// повернеться форматований рядок
